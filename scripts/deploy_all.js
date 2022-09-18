162554a2b16325e7e78fc86d6d278ab15ac5a9be
jaybz <jaybz.e@gmail.com>
2022-09-18 21:50:39 +0800

    Modify deploy_all.js to use command line arguments

--- scripts/deploy_all.js
+++ scripts/deploy_all.js
@@ -28,15 +28,14 @@ export async function main(ns) {
 	}
 
 	const script = args._[0];
+	const script_args = args._.splice(1);
 
 	if (!ns.ls(ns.getHostname()).find(f => f === script)) {
 		ns.tprint(`Script '${script}' does not exist. Aborting.`);
 		return;
 	}
 
-	const script_args = args._.slice(1);
 	const host_list = list_servers(ns).filter(s => ns.hasRootAccess(s)).filter(s => ns.getServerMaxRam(s) > 0).filter(s => !ns.getPurchasedServers().includes(s));
-
 	for(var hostnum = 0; hostnum < host_list.length; hostnum++)	{
 		var host = host_list[hostnum];
 
@@ -47,8 +46,8 @@ export async function main(ns) {
 			ns.tprint(`Killing all scripts on server '${host}'...`);
 			ns.killall(host, true);
 			var threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
-			ns.tprint(`Launching script '${script}' on server '${host}' with ${threads} threads and the following arguments: ${host}`);
-			ns.exec(script, host, threads, host);
+			ns.tprint(`Launching script '${script}' on server '${host}' with ${threads} threads and the following arguments: ${script_args}`);
+			ns.exec(script, host, threads, ...script_args);
 			await ns.sleep(1); // just so we can output "realtime" progress
 		} else {
 			ns.tprint(`Server '${host}' does not exist. Skipped.`);
