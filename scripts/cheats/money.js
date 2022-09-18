74e48cacbf5dc868e98dfb5dfded720790310d72
jaybz <jaybz.e@gmail.com>
2022-09-18 16:48:22 +0800

    Cheat!

---
+++ scripts/cheats/money.js
@@ -0,0 +1,5 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    //eval("root.children[0][Object.keys(root.children[0]).find(k => k.startsWith('__reactFiber'))].child.memoizedProps.player.gainMoney(893401423591579.610000);");
+    eval("root.children[0][Object.keys(root.children[0]).find(k => k.startsWith('__reactFiber'))].child.memoizedProps.player.money = 893401423591579.610000;");
+}
