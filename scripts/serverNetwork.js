0ff7e7cc115f08bb729abbe66f52a7616ba8e011
jaybz <jaybz.e@gmail.com>
2022-09-18 17:27:23 +0800

    Add modified serverNetwork.js scripts

---
+++ scripts/serverNetwork.js
@@ -0,0 +1,133 @@
+import * as hpccWasm from "https://cdn.skypack.dev/@hpcc-js/wasm";
+
+/**
+ * @typedef {Object} ServerNetworkDotStyle
+ * @property {String} graph
+ * @property {String} edge
+ * @property {String} node
+ * @property {String} playerNode
+ * @property {String} rootedNode
+ * @property {String} secureNode
+ */
+
+export class ServerNetwork {
+    /**
+     * @param {import("../NetscriptDefinitions").NS} ns
+     * @param {ServerNetworkDotStyle} style
+     * @param {string} wasmFolder - https://nanodn.github.io/hpccWasm/graphvizlib.wasm
+     */
+    constructor(ns, style, wasmFolder) {
+        /** @type {import("../NetscriptDefinitions").NS} */
+        this.ns = ns;
+        /** @type {ServerNetworkDotStyle} */
+        this.style = style;
+        this.setWasmFolder(wasmFolder);
+    }
+
+    setWasmFolder(wasmFolder) {
+        hpccWasm.wasmFolder(wasmFolder);
+    }
+
+    /** @param {ServerNetworkDotStyle} style */
+    setStyle(style) {
+        this.style = style;
+    }
+
+    getGraph(root = "home") {
+        let adjs = {};
+        const func = (r) => {
+            adjs[r] = this.ns.scan(r).filter((v) => !Object.keys(adjs).includes(v));
+            for (let n of adjs[r]) func(n);
+        };
+        func(root);
+        return adjs;
+    }
+
+    getPath(from, to) {
+        const graph = this.getGraph(from);
+        let queue = [[from]];
+        while (queue.length > 0) {
+            let path = queue.shift();
+            let node = path.slice(-1)[0];
+            if (node === to) return path;
+            if (!Object.keys(graph).includes(node)) return null;
+            for (let adjacent of graph[node]) {
+                let newPath = path.slice();
+                newPath.push(adjacent);
+                queue.push(newPath);
+            }
+        }
+        return null;
+    }
+
+    generateDot(root = "home") {
+        const graph = this.getGraph(root);
+        const playerHackingSkill = this.ns.getHackingLevel();
+        let playerPortCount = 0;
+
+        if (this.ns.fileExists('BruteSSH.exe', 'home'))
+            playerPortCount++;
+
+        if (this.ns.fileExists('FTPCrack.exe', 'home'))
+            playerPortCount++;
+
+        if (this.ns.fileExists('relaySMTP.exe', 'home'))
+            playerPortCount++;
+
+        if (this.ns.fileExists('HTTPWorm.exe', 'home'))
+            playerPortCount++;
+
+        if (this.ns.fileExists('SQLInject.exe', 'home'))
+            playerPortCount++;
+
+        let dot = [];
+        dot.push("strict digraph G {");
+        dot.push(`  graph [${this.style.graph}]`);
+        dot.push(`  edge [${this.style.edge}]`);
+        dot.push(`  node [${this.style.node}]`);
+        const nodeStyles = [
+            this.style.playerNode,
+            this.style.hackableNode,
+            this.style.backdooredNode,
+            this.style.rootedNode,
+            this.style.rootableNode,
+            this.style.secureNode,
+        ];
+        let nodes = [[], [], [], [], [], []],
+            edges = [];
+        Object.entries(graph).forEach(([k, v]) => {
+            const server = this.ns.getServer(k);
+            const hasRootAccess = this.ns.hasRootAccess(k);
+            if (server.purchasedByPlayer) {
+                nodes[0].push(k);
+            } else if (server.backdoorInstalled) {
+                nodes[2].push(k);
+            } else if (hasRootAccess && (server.requiredHackingSkill <= playerHackingSkill)) {
+                nodes[1].push(k);
+            } else if (hasRootAccess) {
+                nodes[3].push(k);
+            } else if (this.ns.getServerNumPortsRequired(k) <= playerPortCount) { // check if rootable
+                nodes[4].push(k);
+            } else {
+                nodes[5].push(k);
+            }
+            if (v.length > 0) edges.push(`  "${k}" -> "${v.sort().join('", "')}"`);
+        });
+        nodes.forEach((v, i) => {
+            if (v.length > 0) {
+                dot.push(`  "${v.sort().join('", "')}" [${nodeStyles[i]}]`);
+            }
+        });
+        dot = dot.concat(edges);
+        dot.push("}");
+        return dot.join("\n");
+    }
+
+    async generateSVGElement(root = "home", layout = "dot") {
+        const dot = this.generateDot(root);
+        /** @type {HTMLDivElement} */
+        const div = eval("document").createElement("div");
+        div.innerHTML = await hpccWasm.graphviz.layout(dot, "svg", layout);
+        return div.children[0];
+    }
+}
---
+++ scripts/visualizeNetwork.js
@@ -0,0 +1,31 @@
+import { ServerNetwork } from "./serverNetwork";
+
+/** @param {import("../NetscriptDefinitions").NS} ns */
+export async function main(ns) {
+  const style = {
+    graph: "bgcolor=transparent pad=0.4",
+    edge: "color=gray len=2 arrowhead=open",
+    node:
+      'color="#222222" fillcolor="#333333" tooltip="Unknown" ' +
+      "fontcolor=white fontname=Courier fontsize=18 " +
+      "margin=0.05 penwidth=4 shape=rectangle style=filled",
+    playerNode: 'color="#224444" fillcolor="#336666" tooltip="Purchased"',
+    hackableNode: 'color="#88AA22" fillcolor="#336633" tooltip="Hackable"',
+    backdooredNode: 'color="#AA4422" fillcolor="#336633" tooltip="Backdoored"',
+    rootedNode: 'color="#224422" fillcolor="#336633" tooltip="Rooted"',
+    rootableNode: 'color="#224422" fillcolor="#666633" tooltip="Rooted"',
+    secureNode: 'color="#442222" fillcolor="#663333" tooltip="Secure"',
+  };
+  const server = ns.args[0] || "home";
+  // you can also download https://nanodn.github.io/hpccWasm/graphvizlib.wasm
+  // and use the absolute path to the download folder
+  const wasmFolder = "https://nanodn.github.io/hpccWasm/";
+  const network = new ServerNetwork(ns, style, wasmFolder);
+  const svg = await network.generateSVGElement(server);
+  const mult = 0.75;
+  svg.setAttribute("width", `${parseInt(svg.getAttribute("width")) * mult}`);
+  svg.setAttribute("height", `${parseInt(svg.getAttribute("height")) * mult}`);
+  await ns.asleep(20); // so the graph appears after clears
+  eval("document").getElementById("terminal").appendChild(svg);
+  ns.tprintf(network.getPath("home", server).join(" -> "));
+}
