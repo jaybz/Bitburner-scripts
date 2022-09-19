b9cd3bd766de2769a90cc8323b4fd8b284c23baa
jaybz <jaybz.e@gmail.com>
2022-09-19 23:18:26 +0800

    Cheaty stuff

--- scripts/cheats/money.js
+++ scripts/cheats/money.js
@@ -2,5 +2,9 @@
 export async function main(ns) {
     var player = root.children[0][Object.keys(root.children[0]).find(k => k.startsWith('__reactFiber'))].child.memoizedProps.player;
     //player.gainMoney(893401423591579.610000);
-    player.money = 8934014235937609812309871579.610000;
+    var cheatValue = 8934014235937609812309871579.610000
+    if (cheatValue > player.money)
+        player.money = cheatValue;
+    else
+        ns.tprint('You don\'t need that!');
 }
