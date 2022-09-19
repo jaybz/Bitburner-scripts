04f95347ff5658d31f0100ed1818014a556c653c
jaybz <jaybz.e@gmail.com>
2022-09-19 21:17:38 +0800

    Add clipboard functionality to network visualizer

--- scripts/visualizeNetwork.js
+++ scripts/visualizeNetwork.js
@@ -3,6 +3,15 @@ import { ServerNetwork } from "./serverNetwork";
 
 /** @param {import(".").NS} ns **/
 export async function main(ns) {
+  const args = ns.flags([["help", false], ["clipboard", false]]);
+  if (args.help) {
+    ns.tprint("This script deploys another script on a server with maximum threads possible.");
+    ns.tprint(`Usage: run ${ns.getScriptName()} HOSTNAME`);
+    ns.tprint("Example:");
+    ns.tprint(`> run ${ns.getScriptName()} n00dles`);
+    return;
+  }
+
   const style = {
     graph: "bgcolor=transparent pad=0.4",
     edge: "color=gray len=2 arrowhead=open",
@@ -17,7 +26,7 @@ export async function main(ns) {
     rootableNode: 'color="#224422" fillcolor="#666633" tooltip="Rooted"',
     secureNode: 'color="#442222" fillcolor="#663333" tooltip="Secure"',
   };
-  const server = ns.args[0] || "home";
+  const server = args._[0] || "home";
   // you can also download https://nanodn.github.io/hpccWasm/graphvizlib.wasm
   // and use the absolute path to the download folder
   const wasmFolder = "https://nanodn.github.io/hpccWasm/";
@@ -29,4 +38,8 @@ export async function main(ns) {
   await ns.asleep(20); // so the graph appears after clears
   eval("document").getElementById("terminal").appendChild(svg);
   ns.tprintf(network.getPath("home", server).join(" -> "));
+  if(args.clipboard) {
+    const path = network.getPath("home", server).join(" ; connect ");
+    await navigator.clipboard.writeText(path);
+  }
 }
